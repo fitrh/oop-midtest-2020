64c638ea658b244e5c749fa0f6f74557bbd71f77
Arsyi Aziz <azizarsyi@gmail.com>
2020-03-24 22:02:04 +0800

    Added several basic user actions

--- H071191003/Banking_System/src/Customer.java
+++ H071191003/Banking_System/src/Customer.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,11 +8,13 @@ public class Customer {
     private int accountNumber;
     private ArrayList<String> accountLog;
     private boolean authenticated = false;
+    private int balance;
 
     public Customer(String username, char[] password, int accountNumber) {
         this.username = username;
         this.password = password;
         this.accountNumber = accountNumber;
+        balance = 0;
     }
 
     public void Authenticate(char[] password) {
@@ -19,5 +22,41 @@ public class Customer {
             authenticated = true;
         }
     }
+
+    public void deposit(int amount) {
+        if (authenticated) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (authenticated) {
+            balance -= amount;
+        }
+    }
+
+    public void logout() {
+        authenticated = false;
+    }
+
+    public void transfer(int amount, Customer recipient) {
+
+    }
+
+    public void changePassword() {
+        if (authenticated) {
+            Console input = System.console();
+            char[] oldPassword = input.readPassword("Input old password : ");
+            if (Arrays.equals(this.password, oldPassword)) {
+                java.util.Arrays.fill(oldPassword, ' ');
+                char[] newPassword = input.readPassword("Input Password : ");
+                password = newPassword;
+                java.util.Arrays.fill(newPassword, ' ');
+            } else {
+                System.out.println("Invalid password!");
+            }
+        }
+    }
+
 
 }
