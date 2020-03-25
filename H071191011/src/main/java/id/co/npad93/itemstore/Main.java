a0086bf5952e3d06efc15e3c92640e6422906461
H071191011 <putramad19h@student.unhas.ac.id>
2020-03-25 19:16:49 +0800

    After discussion, JDBC is discouraged.
    java.(n)io is preferred.

--- H071191011/src/main/java/id/co/npad93/itemstore/Main.java
+++ H071191011/src/main/java/id/co/npad93/itemstore/Main.java
@@ -1,14 +1,8 @@
 package id.co.npad93.itemstore;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main 
 {
     public static void main(String[] args) throws Exception
     {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:itemstore.db.sqlite3");
-        System.out.println("Boo!");
     }
 }
