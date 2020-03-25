4b9afa1b5c066249448dce32684cf39a86eea060
H071191011 <putramad19h@student.unhas.ac.id>
2020-03-25 21:19:41 +0800

    Complete StoreItem

---
+++ H071191011/src/main/java/id/co/npad93/itemstore/StoreItem.java
@@ -0,0 +1,64 @@
+package id.co.npad93.itemstore;
+
+// Item object encapsulated to prevent item stealing
+public class StoreItem
+{
+	// Construct new StoreItem
+	public StoreItem(Item item, int price, Store store)
+	{
+		this.item = item;
+		this.price = price;
+		this.store = store;
+	}
+
+	// Retrieve the name of the item
+	public String getName()
+	{
+		return item.getName();
+	}
+
+	// Retrieve item description
+	public String getDescription()
+	{
+		return item.getDescription();
+	}
+
+	// Retrieve the quantity of the item
+	public int getAmount()
+	{
+		return item.getAmount();
+	}
+
+	// Retrieve the price of the item
+	public int getPrice()
+	{
+		return price;
+	}
+
+	// Buy item
+	public Item buy(User player, int amount)
+	{
+		if (amount > item.getAmount())
+			throw new IllegalArgumentException("item amount < requested amount");
+
+		int totalPrice = price * amount;
+		if (player.getMoney() < totalPrice)
+			throw new IllegalArgumentException("not enough money");
+		
+		player.addMoney(-totalPrice);
+		Item result = item.separate(amount);
+
+		// If item quantity is 0, remove it from shop item list
+		if (item.getAmount() == 0)
+			store.removeItem(this);
+		
+		return result;
+	}
+
+	// Item object
+	private Item item;
+	// Item price
+	private int price;
+	// Store parent object
+	private Store store;
+}
