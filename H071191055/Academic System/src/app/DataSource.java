c3c813edc4d76e427a824e0035d6f72f609f4b0e
richardenrico <richardenrico02@gmail.com>
2020-03-26 11:06:46 +0800

    add DataSource.java untuk membaca file txt

--- H071191055/Academic System/src/app/DataSource.java
+++ H071191055/Academic System/src/app/DataSource.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class DataSource {
 
     private Map<Integer, MataKuliah> matKulMap = new HashMap<>();
-    private Map<Integer, Mahasiswa> mahasiswaMap = new HashMap<>();
+    //private Map<Integer, Mahasiswa> mahasiswaMap = new HashMap<>();
     //private Map<Integer, DosenPA> dosenMap = new HashMap<>();
     
     
@@ -30,53 +30,7 @@ public class DataSource {
 
     }
 
-    public void listMahasiswa() throws IOException {
-        FileReader file = new FileReader("ListMahasiswa.txt");
-        BufferedReader reader = new BufferedReader(file);
-        String tempData[];
-        while (reader.ready()) {
-            tempData = reader.readLine().split(";");
-
-            while (isInteger(tempData[0])) {
-                mahasiswaMap.put(Integer.valueOf(tempData[0]),
-                        new Mahasiswa(Integer.valueOf(tempData[0]), tempData[1], tempData[2]));
-            }
-        }
-        tempData = null;
-        reader.close();
-
-    }
-
-    // public void listDosen() throws IOException {
-    //     FileReader file = new FileReader("ListDosen.txt");
-    //     BufferedReader reader = new BufferedReader(file);
-    //     String tempData[];
-    //     while (reader.ready()) {
-    //         tempData = reader.readLine().split(";");
-
-    //         while (isInteger(tempData[0])) {
-    //             dosenMap.put(Integer.valueOf(tempData[0]),
-    //                     new dosen(Integer.valueOf(tempData[0]), tempData[1], Integer.valueOf(tempData[2])));
-    //         }
-    //     }
-    //     tempData = null;
-    //     reader.close();
-    // }
-        
-
-    public MataKuliah getMataKuliah(Integer key) {
-        return matKulMap.get(key);
-    }
-
-    public Mahasiswa getMahasiswa(Integer key) {
-        return mahasiswaMap.get(key);
-    }
-    
-    // public MataKuliah getMataKuliah(Integer key) {
-    //     return matKulMap.get(key);
-    // }
-
-    public boolean isInteger(String s) {
+        public boolean isInteger(String s) {
         boolean isAnInteger = false;
         try {
             Integer.parseInt(s);
