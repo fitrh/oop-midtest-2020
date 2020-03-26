48e180a5cb71fefd52a6b4ac0e348b7e25178ee9
H071191011 <putramad19h@student.unhas.ac.id>
2020-03-26 13:20:21 +0800

    User class.

--- H071191011/src/main/java/id/co/npad93/itemstore/User.java
+++ H071191011/src/main/java/id/co/npad93/itemstore/User.java
@@ -1,6 +1,53 @@
 package id.co.npad93.itemstore;
 
-// User class
+import java.util.*;
+
+/**
+ * User class represent player or store owner or both
+ */
 public class User
 {
+	public User(String name, int money)
+	{
+		this.name = name;
+		this.money = money;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getMoney()
+	{
+		return money;
+	}
+
+	public void addMoney(int amount)
+	{
+		if ((money + amount) < 0)
+			throw new IllegalArgumentException("money < 0");
+
+		money += amount;
+	}
+
+	public void giveMoney(User player, int amount)
+	{
+		if ((money - amount) < 0)
+			throw new IllegalArgumentException("money < 0");
+		
+		money -= amount;
+		player.money += amount;
+	}
+
+	public Item[] getInventory()
+	{
+		Item[] result = new Item[inventory.size()];
+		inventory.toArray(result);
+		return result;
+	}
+
+	private String name;
+	private int money;
+	private ArrayList<Item> inventory;
 }
