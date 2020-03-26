8db65e5497a1c3db43cd355055bb1da31f3ef4ba
richardenrico <richardenrico02@gmail.com>
2020-03-26 12:05:23 +0800

    minor update MataKuliah.java

--- H071191055/Academic System/src/app/MataKuliah.java
+++ H071191055/Academic System/src/app/MataKuliah.java
@@ -8,12 +8,14 @@ public class MataKuliah {
     private int id;
     private String name;
     private int sks;
+    private String namaDosen;
     // private Map<Integer, DataSource> matKulMap = new HashMap<>();
 
-    public MataKuliah(int id, String name, int sks) {
+    public MataKuliah(int id, String name, int sks, String namaDosen) {
         this.id = id;
         this.name = name;
         this.sks = sks;
+        this.namaDosen = namaDosen;
     }
 
     public int getId() {
