bea53ccebdea5f39b453b36e1fa85ed15cd36121
richardenrico <richardenrico02@gmail.com>
2020-03-26 11:07:38 +0800

    add MataKuliah.java untuk menyimpan data MatKul

--- H071191055/Academic System/src/app/MataKuliah.java
+++ H071191055/Academic System/src/app/MataKuliah.java
@@ -9,12 +9,12 @@ public class MataKuliah {
     private String name;
     private int sks;
     // private Map<Integer, DataSource> matKulMap = new HashMap<>();
-    
+
     public MataKuliah(int id, String name, int sks) {
         this.id = id;
         this.name = name;
         this.sks = sks;
-    } 
+    }
 
     public int getId() {
         return id;
