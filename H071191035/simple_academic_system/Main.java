a1afa74a3cf8c7ef61e00f0b72c79d1712ba65d1
trueazp <54744527+trueazp@users.noreply.github.com>
2020-03-27 18:20:08 +0800

    Create loop & conditioning for mahasiswa's menu
    Membuat perulangan while serta percabangan if-else untuk opsi opsi yang ada di menu untuk sistem user mahasiswa

--- H071191035/simple_academic_system/Main.java
+++ H071191035/simple_academic_system/Main.java
@@ -46,6 +46,76 @@ public class Main {
                     System.out.print("Password => ");
                     password = scan.next();
 
+                    while(login.authMahasiswa(userName, password)) {
+                        System.out.println("--WELCOME MAHASISWA--");
+                        System.out.println("1. Lihat profil");
+                        System.out.println("2. Tambah Mata Kuliah");
+                        System.out.println("3. Hapus Mata Kuliah");
+                        System.out.println("4. Logout");
+                        System.out.print("Masukkan pilihan => ");
+                        a = scan.nextInt();
+
+                        // menampilkan profil mahasiswa
+                        if (a == 1) {
+                            login.statusMahasiswa();
+
+                        // melakukan penambahan mata kuliah
+                        } else if (a == 2) {
+                            System.out.println("--DAFTAR MATA KULIAH--");
+                            System.out.println("1. Matematika Dasar");
+                            System.out.println("2. Bahasa Indonesia");
+                            System.out.println("3. Bahasa Inggris");
+                            System.out.println("4. Pendidikan Agama");
+                            System.out.println("5. Wawasan IPTEKS");
+                            System.out.println("6. Kewarganegaraan");
+                            System.out.print("Masukkan pilihan  => ");
+                            a = scan.nextInt();
+
+                            if (a == 1) {
+                                login.getUser().addMataKuliah(MatDas);
+                            } else if (a == 2) {
+                                login.getUser().addMataKuliah(Bindo);
+                            } else if (a == 3) {
+                                login.getUser().addMataKuliah(English);
+                            } else if (a == 4) {
+                                login.getUser().addMataKuliah(PendAgama);
+                            } else if (a == 5) {
+                                login.getUser().addMataKuliah(Wipteks);
+                            } else if (a == 6) {
+                                login.getUser().addMataKuliah(Pkn);
+                            } else {
+                                System.out.println("Opsi tidak terdapat dalam pilihan...");
+                            }
+
+                        // melakukan penghapusan mata kuliah
+                        } else if (a == 3) {
+                            login.getUser().showMatkul();
+                            System.out.println("Silahkan memilih mata kuliah yang akan dihapus");
+                            System.out.print("Masukkan kode matakuliah  => ");
+                            s = scan.next();
+
+                            if (s.equals(MatDas.getKodeMatkul())) {
+                                login.getUser().removeMataKuliah(MatDas);
+                            } else if (s.equals(Bindo.getKodeMatkul())) {
+                                login.getUser().removeMataKuliah(Bindo);
+                            } else if (s.equals(English.getKodeMatkul())) {
+                                login.getUser().removeMataKuliah(English);
+                            } else if (s.equals(PendAgama.getKodeMatkul())) {
+                                login.getUser().removeMataKuliah(PendAgama);
+                            } else if (s.equals(Wipteks.getKodeMatkul())) {
+                                login.getUser().removeMataKuliah(Wipteks);
+                            } else if (s.equals(Pkn.getKodeMatkul())) {
+                                login.getUser().removeMataKuliah(Pkn);
+                            } else {
+                                System.out.println("Mata kuliah tidak ditemukan...");
+                            }
+
+                        // logout akun mahasiswa
+                        } else {
+                            login.logout();
+                            break;
+                        }
+
                 // login sebagai dosen
                 } else {
 
