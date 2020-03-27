f00aa6bc2e04e5a87a7d406665fea78273156e12
trueazp <54744527+trueazp@users.noreply.github.com>
2020-03-27 18:07:43 +0800

    Create Objects
    Menginstansiasi kelas Login menjadi objek login, serta membuat beberapa objek mata kuliah untuk berperan sebagai mata kuliah yang tersedia

--- H071191035/simple_academic_system/Main.java
+++ H071191035/simple_academic_system/Main.java
@@ -10,7 +10,15 @@ public class Main {
 
     public static void main(String[] args) {
         
-        
+        Login login = Login.getInstance();
+
+        // Mata Kuliah
+        MataKuliah MatDas = new MataKuliah("619", "Matematika Dasar", "Dosen Matematika", 3, 10);
+        MataKuliah Bindo = new MataKuliah("101", "Bahasa Indonesia", "Dosen Bahasa Indoensia", 2, 5);
+        MataKuliah English = new MataKuliah("202", "Bahasa Inggris", "Dosen Bahasa Inggris", 2, 3);
+        MataKuliah PendAgama = new MataKuliah("666", "Pendidikan Agama", "Dosen Agama", 3, 1);
+        MataKuliah Wipteks = new MataKuliah("123", "Wawasan IPTEKS", "Dosen IPTEKS", 2, 3);
+        MataKuliah Pkn = new MataKuliah("555", "Kewarganegaraan", "Dosen Kewarganegaraan", 3, 1);
 
     }
 
