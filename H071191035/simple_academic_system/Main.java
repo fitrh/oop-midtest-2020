15345a48580715c33396b6d41ee91da61ff595de
trueazp <54744527+trueazp@users.noreply.github.com>
2020-03-27 18:05:58 +0800

    Create Main.java
    Membuat kelas Main untuk digunakan sebagai simulasi program sistem akademik sederhana, membuat Scanner, variabel pembantu, serta main method

---
+++ H071191035/simple_academic_system/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * Main
+ */
+public class Main {
+
+    static Scanner scan = new Scanner(System.in);
+    static String s, userName, password;
+    static int a;
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+}
