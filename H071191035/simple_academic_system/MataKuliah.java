42b92339477b341517920e52b499fc5d9ab60151
trueazp <54744527+trueazp@users.noreply.github.com>
2020-03-27 14:02:21 +0800

    Create MataKuliah.java
    Membuat kelas MataKuliah beserta dengan attribute, constructor, dan getternya

---
+++ H071191035/simple_academic_system/MataKuliah.java
@@ -0,0 +1,42 @@
+/**
+ * MataKuliah
+ */
+public class MataKuliah {
+
+    private String kodeMatkul;
+    private String namaMatkul;
+    private String dosenPengajar;
+    private int sks;
+    private int kuota;
+
+    // constructor
+    MataKuliah(String kodeMatkul, String namaMatkul, String dosenPengajar, int sks, int kuota) {
+        this.kodeMatkul = kodeMatkul;
+        this.namaMatkul = namaMatkul;
+        this.dosenPengajar = dosenPengajar;
+        this.sks = sks;
+        this.kuota = kuota;
+    }
+
+    // getter
+    public String getKodeMatkul() {
+        return kodeMatkul;
+    }
+
+    public String getNamaMatkul() {
+        return namaMatkul;
+    }
+
+    public String getDosenPengajar() {
+        return dosenPengajar;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public int getKuota() {
+        return kuota;
+    }
+
+}
