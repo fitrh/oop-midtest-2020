001627a4277c9405f8006b3d2cf2f25d26a3823b
trueazp <54744527+trueazp@users.noreply.github.com>
2020-03-27 14:33:47 +0800

    Create User.java
    Membuat kelas User beserta attribute, constructor, dan getternya

---
+++ H071191035/simple_academic_system/User.java
@@ -0,0 +1,57 @@
+/**
+ * User
+ */
+public class User {
+
+    private int id;
+    private String userName;
+    private String password;
+    private Mahasiswa mahasiswa;
+    private DosenPa dosenPa;
+    private int sksTerdaftar;
+    
+    // constructor
+    public User() {
+        sksTerdaftar = 0;
+    }
+    
+   public User(int id, String userName, String password, Mahasiswa mahasiswa) {
+       this.id = id;
+       this.userName = userName;
+       this.password = password;
+       this.mahasiswa = mahasiswa;
+   }
+
+    public User(int id, String userName, String password, DosenPa dosenPa) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.dosenPa = dosenPa;
+    }
+
+    // getter
+    public int getId() {
+        return id;
+     }
+ 
+     public String getUserName() {
+        return userName;
+     }
+ 
+     public String getPassword() {
+        return password;
+     }
+ 
+     public Mahasiswa getMahasiswa() {
+        return mahasiswa;
+     }
+ 
+     public DosenPa getDosenPa() {
+        return dosenPa;
+     }
+ 
+     public int getSksTerdaftar() {
+        return sksTerdaftar;
+     }
+
+}
