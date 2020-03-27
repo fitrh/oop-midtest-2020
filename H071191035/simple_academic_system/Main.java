736154450b8c2440562a30e7fca718d2a41a8a81
trueazp <54744527+trueazp@users.noreply.github.com>
2020-03-27 18:14:29 +0800

    Create loop & conditioning for main menu
    Membuat perulangan while serta percabangan if-else untuk opsi opsi yang ada di menu utama sistem

--- H071191035/simple_academic_system/Main.java
+++ H071191035/simple_academic_system/Main.java
@@ -20,6 +20,50 @@ public class Main {
         MataKuliah Wipteks = new MataKuliah("123", "Wawasan IPTEKS", "Dosen IPTEKS", 2, 3);
         MataKuliah Pkn = new MataKuliah("555", "Kewarganegaraan", "Dosen Kewarganegaraan", 3, 1);
 
+        while (true) {
+
+            System.out.println("--MENU--");
+            System.out.println("1. Login");
+            System.out.println("2. Exit");
+            System.out.print("Masukkan pilihan => ");
+            a = scan.nextInt();
+
+            // login
+            if (a == 1) {
+
+                System.out.println("--MENU--");
+                System.out.println("1. Login mahasiswa");
+                System.out.println("2. Login dosen");
+                System.out.print("Masukkan pilihan => ");
+                a = scan.nextInt();
+
+                // login sebagai mahasiswa
+                if (a == 1) {
+
+                    System.out.println("--Input your username & password below--");
+                    System.out.print("Username => ");
+                    userName  = scan.next();
+                    System.out.print("Password => ");
+                    password = scan.next();
+
+                // login sebagai dosen
+                } else {
+
+                    System.out.println("--Input your username & password below--");
+                    System.out.print("Username => ");
+                    userName  = scan.next();
+                    System.out.print("Password => ");
+                    password = scan.next();
+
+                }
+
+            // exit
+            } else {
+                break;
+            }
+
+        }
+
     }
 
 }
