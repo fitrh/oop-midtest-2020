f60a465bd490e1c561bd5ba1c1335bf6e1fb280c
SakinahYunus <sakinahys205@gmail.com>
2020-03-29 08:00:29 -0400

    Penambahan Class Air Mineral, Chicken Nugget, dan penerapan Polymorphism

---
+++ H071191046/AirMineral.java
@@ -0,0 +1,11 @@
+class AirMineral extends Addition {
+    public AirMineral() {
+        super(jenis, jumlah, harga);
+        jenis = "Air Mineral";
+        harga = 3.000;
+    }
+    @Override
+    public void showStatus() {
+        super.showStatus();
+    }
+}
---
+++ H071191046/ChickenNugget.java
@@ -0,0 +1,11 @@
+class ChickenNugget extends Addition {
+    public ChickenNugget() {
+        super(jenis, jumlah, harga);
+        jenis = "Chicken Nugget";
+        harga = 15;
+    }
+    @Override
+    public void showStatus() {
+        super.showStatus();
+    }
+}
